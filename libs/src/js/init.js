7f0e0ccb2b227c3bc25ecb091eb15cdf536fa6d7
Tom Hewitt <tom@colourflood.co.uk>
2014-07-19 09:02:57 +0100

    adding in the early stages of animation... realising the mechanic is ugly and broken :(

--- libs/src/js/init.js
+++ libs/src/js/init.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// vars
 var secondsUnits = 0;
 var secondsTens = 0;
 var minutesUnits = 0;
@@ -7,12 +8,20 @@ var minutesTens = 0;
 var hoursUnits = 0;
 var hoursTens = 0;
 
-// start/stop toggle
-var startStop = true;
+// intervals
+var countMiliseconds;
+var countSecondsUnits;
+var countSecondsTens;
+var countMinutesUnits;
+var countMinutesTens;
+var countHoursUnits;
+var countHoursTens;
+
 
 function updateMiliseconds() {
 	var miliseconds = parseInt($('.miliseconds').text(), 10);
     if (miliseconds === 99) {
+    	updateTime('secondsUnits', 9, '.seconds .units');
     	miliseconds = 0;
     } else {
     	miliseconds++;
@@ -45,49 +54,29 @@ $(document).ready(function() {
 	// setup flowtype
 	$('body').flowtype();
 
-	// initial number setup
-	$('.seconds .units').text(secondsUnits);
-	$('.seconds .tens').text(secondsTens);
-	$('.minutes .units').text(minutesUnits);
-	$('.minutes .tens').text(minutesTens);
-	$('.hours .units').text(hoursUnits);
-	$('.hours .tens').text(hoursTens);
-
-
 	// CONTROL FUNCTIONS
 	$('.start-stop').click(function() {
-		if (startStop) {
-
-			startStop = false;
-			$('.start-stop').text('Stop');
-
 			// update miliseconds
-			var countMiliseconds = setInterval(updateMiliseconds, 1);
+			countMiliseconds = setInterval(updateMiliseconds, 1);
 			// update seconds
-			var countSecondsUnits = setInterval(function() { updateTime('secondsUnits', 9, '.seconds .units') }, 1000);
-			var countSecondsTens = setInterval(function() { updateTime('secondsTens', 5, '.seconds .tens') }, 10000);
+			//countSecondsUnits = setInterval(function() { updateTime('secondsUnits', 9, '.seconds .units') }, 1000);
+			countSecondsTens = setInterval(function() { updateTime('secondsTens', 5, '.seconds .tens') }, 10000);
 			// update minutes
-			var countMinutesUnits = setInterval(function() { updateTime('minutesUnits', 9, '.minutes .units') }, 60000);
-			var countMinutesTens = setInterval(function() { updateTime('minutesTens', 5, '.minutes .tens') }, 600000);
+			countMinutesUnits = setInterval(function() { updateTime('minutesUnits', 9, '.minutes .units') }, 60000);
+			countMinutesTens = setInterval(function() { updateTime('minutesTens', 5, '.minutes .tens') }, 600000);
 			// update hours
-			var countHoursUnits = setInterval(function() { updateTime('hoursUnits', 9, '.hours .units') }, 3600000);
-			var countHoursTens = setInterval(function() { updateTime('hoursTens', 5, '.hours .tens') }, 356400000);
-		} else {
-			startStop = false;
-			$('.start-stop').text('Start');
+			countHoursUnits = setInterval(function() { updateTime('hoursUnits', 9, '.hours .units') }, 3600000);
+			countHoursTens = setInterval(function() { updateTime('hoursTens', 5, '.hours .tens') }, 356400000);		
+	});
 
-			// Clear intervals
-			clearInterval(countMiliseconds);
-			clearInterval(countSecondsUnits);
-			clearInterval(countSecondsTens);
-			clearInterval(countMinutesUnits);
-			clearInterval(countMinutesTens);
-			clearInterval(countHoursUnits);
-			clearInterval(countHoursTens);
-		}
-
-
-		
+	$('.stop').click(function() {
+		clearInterval(countMiliseconds);
+		clearInterval(countSecondsUnits);
+		clearInterval(countSecondsTens);
+		clearInterval(countMinutesUnits);
+		clearInterval(countMinutesTens);
+		clearInterval(countHoursUnits);
+		clearInterval(countHoursTens);
 	});
 
 
